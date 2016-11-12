5226d297c5db5ea1d81041f00efe3f93c651ab24
Marco Campulla <mcampulla@gmail.com>
2016-11-12 14:37:04 +0100

    iOS

--- ghostshockey.it.iOS/ViewController.cs
+++ ghostshockey.it.iOS/ViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using ghostshockey.it.core;
+using System.Threading;
 using UIKit;
 
 namespace ghostshockey.it.iOS
@@ -20,6 +21,31 @@ namespace ghostshockey.it.iOS
 			Button.TouchUpInside += delegate {
 				var title = string.Format ("{0} clicks!", count++);
 				Button.SetTitle (title, UIControlState.Normal);
+
+				CancellationTokenSource _cts0 = new CancellationTokenSource();
+				AppController.GetAllYears(
+					// Service call success                 
+					(data) =>
+					{
+						var s = data;
+					},
+					// Service call error
+					(error) =>
+					{
+					//if (error.Contains("confirm"))
+					//    this.VerifyButton.Visibility = ViewStates.Visible;
+
+					//Toast.MakeText(this.Activity.Application, error, ToastLength.Long).Show();
+				},
+					// Service call finished 
+					(exception) =>
+					{
+					//    _isLogginUser = false;
+
+					//// Allow user to tap views
+					//((MainActivity)this.Activity).UnblockUI();
+				});
+
 			};
 		}
 
